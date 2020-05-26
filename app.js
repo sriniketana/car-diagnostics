9aa11b7d431126b778e4c1289b4cd5e1e4076df8
SRIHARI KULKARNI <skulkarni@in.ibm.com>
2020-05-26 12:26:34 +0530

    Add diagnostics API

---
+++ app.js
@@ -0,0 +1,23 @@
+const app = require('express')()
+
+app.get('/', (req, res) => {
+
+  var diagdata = [{
+    "nextServiceDue" : {
+      "date" : "15 May 2020",
+      "KM" : "38400"
+    }, 
+    "vehicleHealth" : {
+      "brakePad" : "GOOD",
+      "oilLevel" : "MEDIUM",
+  
+     },
+     "customerInfo" : {
+       "name" : "Srihari",
+       "vehicleNumber" : "KA04AA1234"
+     }
+  }];
+  res.send(JSON.stringify(diagdata));
+});
+ 
+module.exports.app = app;
